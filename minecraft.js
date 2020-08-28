a70cdb842ca2ea6e2a456828dbea3682ebd62c6e
Atalia Mucharsky <atalia.mucharsky@hotmail.com>
2020-08-28 16:47:26 +0300

    added pickTilefromMap/inventory, pickTool, addTile

--- minecraft.js
+++ minecraft.js
@@ -1,6 +1,10 @@
+
+
+const mapContainer = document.querySelector(".map-container");
+
 function createMap() {
 
-    const mapContainer = document.querySelector(".map-container");
+    // const mapContainer = document.querySelector(".map-container");
     /////creating the matrix and assigning classes to the tile divs/////
     ///will be inside a makeScreen(). this function will be called also when clicking on the reset button///
     const matrix = [
@@ -51,3 +55,90 @@ function createMap() {
 
 createMap();
 // document.body.appendChild(mapContainer);
+
+//////Pick Tile from Map//////
+document.querySelectorAll('.tile').forEach(item => { //event listeners with "pickTileFromMap()" to all tiles but the sky tiles
+    if (item.classList[1] !== "sky") {
+        item.addEventListener('click', pickTileFromMap);
+    }
+});
+
+const currentTile = document.querySelector(".curr-tile"); //inventory tile
+
+function pickTileFromMap(event) {
+    if (currentTool) { // if a tool has been chosen
+        if (event.currentTarget.classList[1] !== "sky" && (tools[currentTool.classList[0]][0] === event.currentTarget.classList[1]) 
+        || tools[currentTool.classList[0]][1] === event.currentTarget.classList[1]) { 
+        // if the clicked tile's [1] class isn't sky && if the theme class in the clicked tile is identical to the tools object value
+            console.log("in if");
+            if (currentTile.classList.length > 1) {
+                currentTile.classList.remove(currentTile.classList[1]);
+            }
+            currentTool.style.border = "5px solid blue";
+            currentTile.classList.add(event.currentTarget.classList[1]); //adding the selected tile class to the inventory
+            event.currentTarget.classList.remove(event.currentTarget.classList[1]); //removing theme-class and adding class 'sky' to the selected tile from map
+            event.currentTarget.classList.add("sky");
+            event.currentTarget.addEventListener("click", addTileToMap);//now we can add tiles from the inventory to this spot as well
+            currentTile.style.border = "5px solid grey"; //returning the currTile border color to usual in case it was pushed
+        }
+        else {
+            currentTool.style.border = "5px solid red";
+        }
+    }
+    
+}
+
+
+////Pick Tile from Inventory & Add tile from inventory to map////
+currentTile.addEventListener("click", pickTileFromInventory);
+
+document.querySelectorAll('.tile').forEach(item => { //event listeners with "pickTileFromInventory()" to all sky tiles
+    if (item.classList[1] === "sky") {
+        item.addEventListener('click', addTileToMap);
+    }
+});
+
+let currentTileChecked = 0; //determines if the user click on the inventory tile
+
+function pickTileFromInventory(event) {
+    if (event.currentTarget.classList.length > 1) { //if the inventory isn't empty
+        currentTile.style.border = "5px solid blue";
+        currentTileChecked = 1; //now we can add the tile to the map
+    }
+    else {
+        currentTile.style.border = "5px solid red";
+    }
+}
+
+function addTileToMap(event) {
+    if (currentTile.classList.length > 1 && currentTileChecked && event.currentTarget.classList[1] === "sky") {
+        event.currentTarget.classList.remove(event.currentTarget.classList[1]);
+        event.currentTarget.classList.add(currentTile.classList[1]);
+        currentTile.classList.remove(currentTile.classList[1]);
+        event.currentTarget.removeEventListener("click", addTileToMap); //removing the eventlistener of adding tile to this spot
+        event.currentTarget.addEventListener("click", pickTileFromMap); //adding eventlistener of picking tile from this spot
+        currentTile.style.border = "5px solid grey";
+        currentTileChecked = 0;
+    }
+}
+
+const tools = { "pickaxe": ["rocks"], "shovel": ["dirt"], "axe": ["wood","leaves"],};
+const pickaxe = document.querySelector(".pickaxe img");
+const shovel = document.querySelector(".shovel img");
+const axe = document.querySelector(".axe img");
+console.log(tools);
+let currentTool;
+
+
+function pickTool(event) {
+    currentTool = event.currentTarget;
+    currentTool.style.border = "5px solid blue";
+    console.log(currentTool);
+}
+
+pickaxe.addEventListener("click", pickTool);
+shovel.addEventListener("click", pickTool);
+axe.addEventListener("click", pickTool);
+
+
+
